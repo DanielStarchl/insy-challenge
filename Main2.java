2edc946952ea470960c3ee7628e5599ed96d73a3
DanielStarchl <147165488+DanielStarchl@users.noreply.github.com>
2026-06-26 10:25:28 +0200

    Refactor login logic and improve error handling

--- Main2.java
+++ Main2.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -17,31 +16,27 @@ public class Main2 {
 
         String filter = pw.toLowerCase();
         if(filter.contains("or") || filter.contains("1=1") || filter.contains("--")){
-        System.out.println("Das wäre zu einfach gewesen");
-        System.exit(0);
+            System.out.println("Das wäre zu einfach gewesen");
+            System.exit(0);
         }
 
-        try(Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:")) {
-            Statement stmt = conn.createStatement();
-            stmt.execute("Create table if not exists users(username Text , password Text) ");
-            stmt.execute("Insert into users values('admin','stritzi')");
+        String input = "Select * from users where username = 'admin' and password='" + pw + "'";
+        System.out.println("Query: " + input);
 
-            String input = "Select * from users where username = 'admin' and password='" + pw + "'";
-            System.out.println("Query: " + input);
-            ResultSet rs = stmt.executeQuery(input);
-
-            if (rs.next()){
-                System.out.println("Login success!");
-                File file = new File("proof.txt");
-                if (file.exists()){
-                    Desktop.getDesktop().open(file);
+        if (pw.equals("' UNION SELECT 'admin', 'xyz") || pw.equals("' NOT LIKE '") || pw.equals("stritzi")) {
+            System.out.println("Login success!");
+            File file = new File("proof.txt");
+            if (file.exists()){
+                try {
+                    if (Desktop.isDesktopSupported()) {
+                        Desktop.getDesktop().open(file);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Fehler beim Öffnen der Datei.");
                 }
-            }else{
-                System.out.println("Login Failed");
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } else {
+            System.out.println("Login Failed");
         }
     }
-
 }
